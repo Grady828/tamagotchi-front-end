8264ac8bee050fb36ba54bae140c449e24c266ba
eight20eight@gmail.com <email-address-you-used-at-github@example.com>
2021-02-28 11:25:14 -0500

    now tracking user input via the makeANewPet state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { Link, Route, Switch } from 'react-router-dom'
 
 export function App() {
-  useEffect(function () {
-    console.log('Run once at start')
+  const [pets, setPets] = useState({})
+  const [newPet, setNewPet] = useState('')
+
+  useEffect(async function () {
+    const response = await axios.get(
+      'https://tamagotchimm.herokuapp.com/api/Pets'
+    )
+    setPets(response.data)
   }, [])
 
   return (
@@ -31,9 +38,25 @@ export function App() {
         </nav> */}
       </header>
       <main>
-        <nav></nav>
+        <nav>
+          <ul>
+            {Object.entries(pets).map(([petCode, petDetails]) => {
+              return <li key={petDetails.id}>{petDetails.name}</li>
+            })}
+          </ul>
+          <form>
+            <input
+              type="text"
+              placeholder="Make a new pet"
+              value={newPet}
+              onChange={function (event) {
+                setNewPet(event.target.value)
+              }}
+            />
+          </form>
+        </nav>
       </main>
-      <Switch>
+      {/* <Switch>
         <Route exact path="/">
           Home
         </Route>
@@ -44,7 +67,7 @@ export function App() {
           Page 2
         </Route>
         <Route path="*">Not Found</Route>
-      </Switch>
+      </Switch> */}
     </header>
   )
 }
